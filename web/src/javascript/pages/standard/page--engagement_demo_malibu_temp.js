9c6c6b83794e22871cc9f07c9a469ee08d3095ac
Kaitlyn Noone <knoone@vipasolutions.com>
2015-12-04 17:31:33 -0600

    Changes to edit more Malibu engagement data

--- web/src/javascript/pages/standard/page--engagement_demo_malibu_temp.js
+++ web/src/javascript/pages/standard/page--engagement_demo_malibu_temp.js
@@ -1,20 +1,62 @@
 jQuery(function($) {
-
     var $engagementCont = $('.view-engagement'),
         $engagementName = $engagementCont.find('.engagement-name'),
-        $postEventSurveyCon = $('.post-event-survey');
+        $preEventSurveyCon = $('.pre-event-survey'),
+        $rsvpSurveyCon = $('rsvp'),
+        $inMarketSurveyCon = $('.in-market-survey'),
+        $postEventSurveyCon = $('.post-event-survey'),
+        $leadEmailCon = $('.lead-email'),
+        $leadPhoneCon = $('.lead-phone'),
+        $leadSocialCon = $('.lead-social'),
+        $leadAddressCon = $('.lead-address');
+
 
     if ($engagementName.text() == 'Malibu Example') {
-        var $externalSurveyName = $postEventSurveyCon.find(".survey-item-name");
-        var $externalSurveyCount = $postEventSurveyCon.find(".survey-item-data-count");
-        var $externalSurveyActions = $postEventSurveyCon.find(".survey-item-actions");
 
-        $externalSurveyName.attr("href", "#");
-        $externalSurveyCount.html('487');
+        function resetSurveyCounts() {
+            var $preEventSurveyCount = $preEventSurveyCon.find(".survey-item-data-count"),
+                $rsvpSurveyCount = $rsvpSurveyCon.find(".survey-item-data-count"),
+                $inMarketSurveyCount = $inMarketSurveyCon.find(".survey-item-data-count"),
+                $externalSurveyCount = $postEventSurveyCon.find(".survey-item-data-count");
 
-        $externalSurveyActions
-            .append($('<a class="btn btn-glyph-only btn-xsmall btn-download" href="/demo2/survey/results" title="View Results"><span class="btn-text">View Results</span></a>'))
-            .append($('<a class="btn btn-glyph-only btn-xsmall btn-download" href="#" title="Manage Results"><span class="btn-text">Manage Results</span></a>'));
+            $preEventSurveyCount.html('243');
+            $rsvpSurveyCount.html('602');
+            $inMarketSurveyCount.html('237');
+            $externalSurveyCount.html('487');
+        }
+
+        function resetLeadCounts() {
+            var $leadEmailCount = $leadEmailCon.find(".lead-count"),
+                $leadPhoneCount = $leadPhoneCon.find(".lead-count"),
+                $leadSocialCount = $leadSocialCon.find(".lead-count"),
+                $leadAddressCount = $leadAddressCon.find(".lead-count");
+
+            $leadEmailCount.html('620');
+            $leadPhoneCount.html('503');
+            $leadSocialCount.html('254');
+            $leadAddressCount.html('487');
+        }
+
+        function changePostEventSurvey() {
+            var $externalSurveyHeader= $postEventSurveyCon.find(".survey-item-header"),
+                $externalSurveyHeaderName = $('<div class="survey-item-name">Post Event Survey</div>'),
+                $externalSurveyActions = $postEventSurveyCon.find(".survey-item-actions");
+
+            //Remove Link from post-event survey name
+            $externalSurveyHeader.html($externalSurveyHeaderName);
+
+            //Append extra action buttons to post-event survey
+            $externalSurveyActions
+                .append($('<a class="btn btn-glyph-only btn-xsmall btn-view" href="/demo2/survey/results" title="View Results"><span class="btn-text">View Results</span></a>'))
+                .append($('<a class="btn btn-glyph-only btn-xsmall btn-edit" href="#" title="Manage Results"><span class="btn-text">Manage Results</span></a>'));
+        }
+
+
+        resetSurveyCounts();
+        resetLeadCounts();
+        changePostEventSurvey();
+
+
     }
 
 });
